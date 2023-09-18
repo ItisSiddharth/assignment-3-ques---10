6e558e455fb94a41387f3d85eb49cc2e7fea91c3
sidharth <sidharthkumar@gmail.com>
2023-09-18 15:07:59 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿
+
+
+Assembly1 -
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+[assembly:InternalsVisibleTo("As3")]
+
+namespace Assignment3
+{
+    internal class Program
+    {
+        internal String name;
+        internal Program(String name)
+        {
+            this.name = name;
+        }
+        protected internal void getName()
+        {
+            Console.WriteLine(name);
+        }
+
+    }
+
+}
+
+Assembly2 -
+
+using Assignment3;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace As3
+{
+    internal class Program1
+    {
+        static void Main(string[] args)
+        {
+            Assignment3.Program program = new Assignment3.Program("sidharth");
+            program.getName();
+            Console.ReadLine();
+        }
+    }
